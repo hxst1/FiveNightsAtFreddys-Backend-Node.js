b10f66a642e0e53775bef3d6e4731bdbebb1dc12
Edu Ruiz <eya@MacBook-Air-de-Edu.local>
2022-02-19 00:13:28 +0100

    :sparkles: root conections , errors and init

---
+++ src/index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const debug = require("debug")("robots: root");
+const chalk = require("chalk");
+
+const initialServer = require("./server");
+const conectMongoDB = require("./db");
+
+const port = process.env.SERVER_PORT || 4444;
+const mongoDB = process.env.MONGO_SRING;
+
+(async () => {
+  try {
+    await conectMongoDB(mongoDB);
+
+    await initialServer(port);
+  } catch (error) {
+    debug(chalk.bgRed.black(`Error: ${error.message}`));
+  }
+})();
