e31a1f445282c6e3a9a90e0f9bc637fc872f4790
Edu Ruiz <eya@MacBook-Air-de-Edu.local>
2022-02-20 18:54:35 +0100

    :fire: method post, put and delete added

--- src/server/routers/ robotsRouter.js
+++ src/server/routers/ robotsRouter.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const cors = require("cors");
-const { getAllRobots, getRobot } = require("../controllers/robotsControllers");
+const {
+  getAllRobots,
+  getRobot,
+  createRobot,
+  updateRobot,
+  deleteRobot,
+} = require("../controllers/robotsControllers");
 
 const router = express.Router();
+const tokenValidator = require("../middlewares/tokenValidator");
 
 router.get("/", cors(), getAllRobots);
 router.get("/:idRobot", cors(), getRobot);
+router.post("/create", tokenValidator, createRobot);
+router.put("/update", tokenValidator, updateRobot);
+router.delete("/delete/:idRobot", tokenValidator, deleteRobot);
 
 module.exports = router;
